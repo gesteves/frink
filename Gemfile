c8f364483ff0f996a87341b7c1e19dc0c8d12e97
Guillermo Esteves <g@gesteves.com>
2016-02-24 08:11:35 -0500

    Remove redis

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ ruby "2.3.0"
 
 gem "sinatra"
 gem "thin"
-gem "redis"
 gem "httparty"
 gem "dotenv"
 gem "foreman"
--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'json'
 require 'httparty'
-require 'redis'
 require 'dotenv'
 require 'uri'
 require 'text'
